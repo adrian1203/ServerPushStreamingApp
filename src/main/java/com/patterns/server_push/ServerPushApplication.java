d932e4652d07c42aedbdd291a47d050df1f2b324
Adrian Opiela <adrian.opiela11@gmail.com>
2019-01-05 18:15:47 +0100

    initial commit

--- src/main/java/com/patterns/server_push/ServerPushApplication.java
+++ src/main/java/com/patterns/server_push/ServerPushApplication.java
@@ -1,13 +1,13 @@
-package com.patterns.demo;
+package com.patterns.server_push;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class ServerPushApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(ServerPushApplication.class, args);
     }
 
 }
--- src/test/java/com/patterns/server_push/ServerPushApplicationTests.java
+++ src/test/java/com/patterns/server_push/ServerPushApplicationTests.java
@@ -1,4 +1,4 @@
-package com.patterns.demo;
+package com.patterns.server_push;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DemoApplicationTests {
+public class ServerPushApplicationTests {
 
     @Test
     public void contextLoads() {
