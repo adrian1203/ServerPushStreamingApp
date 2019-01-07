7fd05b6738aeefb2f9190783a9f2f60800128e66
kubas1129 <kubas11513@gmail.com>
2019-01-07 21:49:20 +0100

    Add library

---
+++ src/main/java/com/patterns/server_push/ServerPushApplication.java
@@ -0,0 +1,16 @@
+package com.patterns.server_push;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerPushApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerPushApplication.class, args);
+
+
+    }
+
+}
+
---
+++ src/main/java/com/patterns/server_push/controllers/TestController.java
@@ -0,0 +1,25 @@
+package com.patterns.server_push.controllers;
+
+
+import config.AppConfig;
+import core.SessionHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/")
+public class TestController {
+
+    @CrossOrigin
+    @GetMapping (path = "test")
+    public String test() {
+        return "test";
+    }
+
+    @Autowired
+    private SessionHandler sessionHandler;
+
+}
