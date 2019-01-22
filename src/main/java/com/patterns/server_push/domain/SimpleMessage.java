931cf82358a25b591d6289ffc19dbd14e2a3d82e
Adrian Opiela <adrian.opiela11@gmail.com>
2019-01-22 11:55:10 +0100

    add connect to DB

--- src/main/java/com/patterns/server_push/domain/SimpleMessage.java
+++ src/main/java/com/patterns/server_push/domain/SimpleMessage.java
@@ -6,15 +6,30 @@ import lombok.Setter;
 import lombok.ToString;
 import notification.Notification;
 
+import javax.persistence.*;
 import java.util.Date;
 
 @Getter
 @Setter
 @ToString
+@Entity
+@Table(name = "simple_message")
 public class SimpleMessage implements Notification {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
+    @SequenceGenerator(name = "sequenceGenerator")
+    private Long id;
+
+    @Column(name = "sender")
     private String sender;
+
+    @Column(name = "receiver")
     private String receiver;
+
+    @Column(name = "text")
     private String text;
+
+    @Column(name = "date")
     private Date date;
 }
---
+++ src/main/java/com/patterns/server_push/repository/SimpleMessageRepository.java
@@ -0,0 +1,9 @@
+package com.patterns.server_push.repository;
+
+import com.patterns.server_push.domain.SimpleMessage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SimpleMessageRepository  extends JpaRepository<SimpleMessage, Long> {
+}
